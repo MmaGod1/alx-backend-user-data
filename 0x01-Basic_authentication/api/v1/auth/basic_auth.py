c8606c62ea85182c28fb14718be87f5a2cb2dba3
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-15 14:21:29 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Authentication Module.
 """
-from .auth import Auth
+from api.v1.auth.auth import Auth
 import base64
 from models.user import User
 from typing import TypeVar
