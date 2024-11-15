0c4ff5345376696dcab008ab397b2910fdc8a0d3
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-15 15:12:53 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -74,7 +74,8 @@ class BasicAuth(Auth):
         user_email, pswd = decoded_base64_authorization_header.split(':', 1)
         return (user_email, pswd)
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> UserType:
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str) -> UserType:
         """
         Returns a User instance based on user_email and user_pwd.
         """
@@ -104,23 +105,22 @@ class BasicAuth(Auth):
         auth_header = self.authorization_header(request)
         if auth_header is None:
             return None
-        
+
         # Extract the Base64 part from the Authorization header
         base64_auth = self.extract_base64_authorization_header(auth_header)
         if base64_auth is None:
             return None
-        
+
         # Decode the Base64 string
         decoded_auth = self.decode_base64_authorization_header(base64_auth)
         if decoded_auth is None:
             return None
-        
+
         # Extract user email and pswd from the decoded string
         user_email, user_pwd = self.extract_user_credentials(decoded_auth)
         if user_email is None or user_pwd is None:
             return None
-        
+
         # Retrieve the User object from email and pswd
         user = self.user_object_from_credentials(user_email, user_pwd)
         return user
-
