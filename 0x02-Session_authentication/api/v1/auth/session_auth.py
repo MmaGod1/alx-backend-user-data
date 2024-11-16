8870582e93f6325f27c52d2b2a8ec9e8377e5dbb
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-16 17:24:07 +0100

    Import uuid, use conditions properly

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,19 +2,18 @@
 """ Session Authentication Module.
 """
 from .auth import Auth
+import uuid
 
-
-class SessionAuth():
+class SessionAuth:
     """ Session Authentication mechanism."""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Creates the session ID"""
-        if user_id is None:
-            return None
-        if isinstance(user_id, str) is None:
+        """Create a session ID for a given user ID."""
+        if user_id is None or not isinstance(user_id, str):
             return None
 
-        session_id = uuid.uuid4()
-        user_id_by_session_id[session_id] = user_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
         return session_id
