26dead59229c5b6e20bef57669d7a81bf4929e2d
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-16 17:18:39 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,17 @@
 from .auth import Auth
 
 
-class SessionAuth(Auth):
+class SessionAuth():
     """ Session Authentication mechanism."""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates the session ID"""
+        if user_id is None:
+            return None
+        if isinstance(user_id, str) is None:
+            return None
+
+        session_id = uuid.uuid4()
+        user_id_by_session_id[session_id] = user_id
+        return session_id
