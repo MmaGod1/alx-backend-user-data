c6393137fee050701f00c21431e03897dfd22d47
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-16 18:38:12 +0100

    Fixing indentation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,7 +45,7 @@ def before_request():
 
     # Check for authorization header or session cookie
     if auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None:
+    auth.session_cookie(request) is None:
         abort(401)
 
     # Assign current user to request.current_user
