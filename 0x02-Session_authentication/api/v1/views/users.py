6e8a50b409f0b881b09a4e96d2d786bfadc4e0dc
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-17 11:32:39 +0100

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -28,6 +28,7 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
+    if user_id is None:
         abort(404)
 
     # Handle 'me' as the user_id
@@ -35,11 +36,13 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         user = request.current_user
-        return jsonify(request.current_user.to_json())
+        return jsonify(user.to_json())
 
     # Default behavior
     user = User.get(user_id)
     if user is None:
+        abort(404)
+    if request.current_user is None:
         abort(404)
     return jsonify(user.to_json())
 
