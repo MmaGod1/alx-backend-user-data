519371d80efe48c6d678e178dfd587d2093e3606
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-17 13:57:38 +0100

    Pycodestyle check

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -40,7 +40,8 @@ def session_auth_login():
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def session_logout():
     """ Handle user logout. """
     from api.v1.app import auth
