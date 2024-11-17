40a3333ccebc2d985e880531bbffd1d8646311fa
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-17 12:49:59 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,6 +47,7 @@ def before_request():
     if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
+    request.current_user = auth.current_user(request)
     # Check for current user
     if auth.current_user(request) is None:
         abort(403)
