973abc8d306fda47b09f6cd8a6d428c9b230ba4c
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-17 01:30:16 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -42,6 +42,7 @@ class SessionAuth(Auth):
         return User.get(user_id)
 
     def destroy_session(self, request=None):
+        """ Deleted the user session/logout. """
         if request is None:
             return False
 
