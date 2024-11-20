558e2ea97014c067fab454a875f686a2cea20c21
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-20 16:54:56 +0100

    Add type annotations

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Adds new users to the database
         Args: email (str) users email
               hashed_password (str) user hashed password
