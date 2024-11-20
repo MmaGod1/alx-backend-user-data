00ee6311e58bbabebe9d2e1e1f251e144086e4db
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-20 22:53:24 +0100

    Indentation fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -114,15 +114,17 @@ def update_password():
     The request must include "email", "reset_token", and
     "new_password" in the form data.
     """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
     try:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
 
     return jsonify({"email": f"{email}", "message": "Password updated"})
+
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
