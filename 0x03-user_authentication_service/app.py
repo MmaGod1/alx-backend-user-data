f950b29e3ecbd96344435c92fb98d09aef53e527
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-20 22:30:58 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,5 +89,22 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """
+    Handle POST /reset_password route
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+
+    try:
+        # Generate reset token
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        # Email not registered
+        abort(403)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
