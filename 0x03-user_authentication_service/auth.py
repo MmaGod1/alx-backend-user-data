c323c5942c635cfda881836aac7c4b72cce0fad2
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-20 21:06:51 +0100

    Fix indentation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,8 +24,8 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 def _generate_uuid(self) -> str:
-        """Generates a new UUID and returns it as a string."""
-        return str(uuid4())
+    """Generates a new UUID and returns it as a string."""
+    return str(uuid4())
 
 
 class Auth:
